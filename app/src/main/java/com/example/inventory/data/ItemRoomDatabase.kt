125a40b1b3bfbab4cfe18ba2ad062e53333f57a1
dqvuong2111 <dqvuong2111@gmail.com>
2025-12-28 15:05:14 +0700

    create inventory viewmodel

--- app/src/main/java/com/example/inventory/data/ItemRoomDatabase.kt
+++ app/src/main/java/com/example/inventory/data/ItemRoomDatabase.kt
@@ -5,8 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-class ItemRoomDatabase {
-    @Database (entities = [Item::class], version = 1, exportSchema = false)
+@Database (entities = [Item::class], version = 1, exportSchema = false)
     abstract class ItemRoomDatabase : RoomDatabase(){
         abstract fun itemDao() : ItemDao
 
@@ -27,4 +26,3 @@ class ItemRoomDatabase {
             }
         }
     }
-}
